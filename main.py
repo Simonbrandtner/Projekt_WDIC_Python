09876f40d70efbb346a190cc24465d8e15adce23
Simonbrandtner <151824278+Simonbrandtner@users.noreply.github.com>
2024-01-16 18:13:46 +0100

    test

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ except ModuleNotFoundError:
     print("""Vous n'avez pas téléchargé le module pygame ! \n
         Téléchargez le avec la commande ci-contre : pip install pygame""")
 from functions import animate_gif, play_sound
+test
 
 while PLAYING:
     ACCELERATION = SCORE / 2
